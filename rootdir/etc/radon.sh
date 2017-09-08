23ac5897d32103c16bc1b248fbf4cdb042ecf6e0
gismo <koumparilos@gmail.com>
2017-09-08 20:27:24 +0300

    Update default kernel config

--- rootdir/etc/radon.sh
+++ rootdir/etc/radon.sh
@@ -22,7 +22,8 @@
         echo "18432,23040,27648,32256,55296,80640" > /sys/module/lowmemorykiller/parameters/minfree
     else
         echo 1 > /sys/module/lowmemorykiller/parameters/enable_adaptive_lmk
-        echo "14746,18432,22118,25805,40000,55000" > /sys/module/lowmemorykiller/parameters/minfree
+        echo "16384,20992,24064,30720,46080,66560" > /sys/module/lowmemorykiller/parameters/minfree
+	echo 10 > /proc/sys/vm/dirty_background_ratio
     fi
 
 	Mode=`cat /init.radon.rc | grep zrammode`
@@ -32,4 +33,7 @@
 	mkswap /dev/block/zram0
 	swapon /dev/block/zram0
 	fi
-	
+
+#HEADPHONE GAIN
+echo "3 3" > sys/kernel/sound_control/headphone_gain	
+
